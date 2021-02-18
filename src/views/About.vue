04035b4b996f095767806f36be8961a14e8830cb
Peter Jang <peter.x.jang@gmail.com>
2021-02-17 20:31:49 -0600

    Add multi-select component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <multiselect v-model="value" :options="options" :multiple="true"></multiselect>
+    <p>The value is {{ value }}</p>
   </div>
 </template>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<script>
+import Multiselect from "vue-multiselect";
+
+export default {
+  components: { Multiselect },
+  data() {
+    return {
+      value: null,
+      options: ["list", "of", "options"],
+    };
+  },
+};
+</script>
